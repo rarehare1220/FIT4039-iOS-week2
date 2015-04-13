87a47f6925bec8b1a9c84a0247de0d17cf8f7474
double <double@doubletekiMacBook-Pro-3.local>
2015-04-13 15:06:56 +1000

    Initial Commit

---
+++ week2tute/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  week2tute
+//
+//  Created by double on 13/04/2015.
+//  Copyright (c) 2015 double. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
